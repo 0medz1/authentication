4f12c5fd78deebd84837f08343a978f1aec7a99f
0medz1 <98036546+0medz1@users.noreply.github.com>
2024-01-15 13:30:53 -0500

    Add files via upload

---
+++ Popup.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace authentification.Pages
+{
+    public partial class Popup:ComponentBase
+    {
+        [Parameter]
+        [SupplyParameterFromQuery]
+        public int? Age { get; set; }
+        [Parameter]
+        [SupplyParameterFromQuery]
+        public string? Email { get; set; }
+        [Parameter]
+        [SupplyParameterFromQuery]
+        public string? Name { get; set; }
+        
+    }
+}
---
+++ Test.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace authentification.Pages
+{
+    public partial class Test: ComponentBase
+    {
+    }
+    public class Person
+    {
+        [Parameter]
+        [Required]
+        public string Name { get; set; }
+        [Parameter]
+        [Range(18, 80, ErrorMessage = "Age must be between 18 and 80.")]
+        public int Age { get; set; }
+        [Parameter]
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$", ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+
+
+    }
+}
